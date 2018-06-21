3c93123354348af7f5cd2de1b0ae8e9d96eb6e31
Xander Ge <xander.q.ge@gmail.com>
2018-06-21 14:20:37 -0500

    Added Conversion method and View Setup

--- ConversionServer/ConversionServer/Controllers/TemperatureController.cs
+++ ConversionServer/ConversionServer/Controllers/TemperatureController.cs
@@ -11,9 +11,14 @@ namespace ConversionServer.Controllers
     public class HomeController1 : Controller
     {
         // GET: /<controller>/
-        public IActionResult Index()
+        public ActionResult ConvertToF()
         {
-            return View();
+            return View(viewName:"TemperatureConversion", model: 0.0);
+        }
+
+        public ActionResult ConvertToF(double degreesC)
+        {
+            return View(viewName: "TemperatureConversion", model: (degreesC * 9 / 5 + 32);
         }
     }
 }
