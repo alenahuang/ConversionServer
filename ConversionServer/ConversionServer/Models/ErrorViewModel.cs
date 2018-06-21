40c94f588d16415b31d9cad01a3e445999921796
Alena Huang <alenahuang1@gmail.com>
2018-06-21 13:01:14 -0500

    Initial

---
+++ ConversionServer/ConversionServer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ConversionServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
