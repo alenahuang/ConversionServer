565d76e63f755d2e9d187a121a9c85de46a8ecde
SamzFerg <SamzFerg@gmail.com>
2018-06-21 14:27:32 -0500

    Added Ouput Areas
    Added input tags in right side then disabled them to not allow more input

--- ConversionServer/ConversionServer/Controllers/DigitalController.cs
+++ ConversionServer/ConversionServer/Controllers/DigitalController.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ConversionServer.Models;
 
 namespace ConversionServer.Controllers
 {
     public class DigitalController : Controller
     {
-        public IActionResult Index()
+        DigitalModel model = new DigitalModel();
+
+        public ActionResult DigitalConversion()
         {
-            return View();
+            model.inputNumber = 0;
+            model.outputNumber = 0;
+            return View(viewName: "DigitalConversion", model: model);
         }
     }
 }
---
+++ ConversionServer/ConversionServer/Models/DigitalModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConversionServer.Models
+{
+    public class DigitalModel
+    {
+        public double MegaBytes { get; set; }
+        public double Gigabytes { get; set; 
+    }
+}
