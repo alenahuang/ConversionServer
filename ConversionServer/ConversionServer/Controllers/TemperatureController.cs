44a5a26aa4e41af54b38e89bc6bdb4e7e363a599
Xander Ge <xander.q.ge@gmail.com>
2018-06-21 14:33:02 -0500

    Fixed Typos

--- ConversionServer/ConversionServer/Controllers/TemperatureController.cs
+++ ConversionServer/ConversionServer/Controllers/TemperatureController.cs
@@ -8,9 +8,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace ConversionServer.Controllers
 {
-    public class HomeController1 : Controller
+    public class TemperatureController : Controller
     {
-        // GET: /<controller>/
         public ActionResult ConvertToF()
         {
             return View(viewName:"TemperatureConversion", model: 0.0);
@@ -18,7 +17,7 @@ namespace ConversionServer.Controllers
 
         public ActionResult ConvertToF(double degreesC)
         {
-            return View(viewName: "TemperatureConversion", model: (degreesC * 9 / 5 + 32);
+            return View(viewName: "TemperatureConversion", model: (degreesC * 9 / 5 + 32));
         }
     }
 }
